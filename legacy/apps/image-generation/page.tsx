a33fc9c1b2f0120146f2b17b0506035d7e38d576
Pancham <{panchamb63@gmail.com}+{Pancham555}@users.noreply.github.com>
2025-03-07 12:42:14 +0530

    Tried to resolve some issues

--- legacy/apps/image-generation/page.tsx
+++ legacy/apps/image-generation/page.tsx
@@ -1,10 +1,10 @@
-import { ImageGenerator } from "../image-generator";
-import React from "react";
+// import { ImageGenerator } from "../image-generator";
+// import React from "react";
 
-export default function ImageGeneration() {
-  return (
-    <div>
-      <ImageGenerator />
-    </div>
-  );
-}
+// export default function ImageGeneration() {
+//   return (
+//     <div>
+//       <ImageGenerator />
+//     </div>
+//   );
+// }
