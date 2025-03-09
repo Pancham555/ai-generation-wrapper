1d3757da6a33b88ac6ff5de07e40486816b24eaf
Pancham <{panchamb63@gmail.com}+{Pancham555}@users.noreply.github.com>
2025-03-09 16:03:06 +0530

    Made some imporvements

--- src/components/nav-main.tsx
+++ src/components/nav-main.tsx
@@ -1,28 +1,28 @@
 "use client";
 
 import {
-  ChevronRight,
+  // ChevronRight,
   Image,
   MessageSquare,
   BarChart3,
-  Wand2,
+  // Wand2,
   type LucideIcon,
 } from "lucide-react";
 
-import {
-  Collapsible,
-  CollapsibleContent,
-  CollapsibleTrigger,
-} from "@/components/ui/collapsible";
+// import {
+//   Collapsible,
+//   CollapsibleContent,
+//   CollapsibleTrigger,
+// } from "@/components/ui/collapsible";
 import {
   SidebarGroup,
   SidebarGroupLabel,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
-  SidebarMenuSub,
-  SidebarMenuSubButton,
-  SidebarMenuSubItem,
+  // SidebarMenuSub,
+  // SidebarMenuSubButton,
+  // SidebarMenuSubItem,
 } from "@/components/ui/sidebar";
 import Link from "next/link";
 
